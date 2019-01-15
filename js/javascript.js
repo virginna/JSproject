95c4c9b49fa01cebd6c6caf63d7c64f854893564
virginna <45034487+virginna@users.noreply.github.com>
2019-01-15 17:13:20 +0200

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -9,13 +9,26 @@ function newElement() {
 }
 
 
-let delBtn, delTask;
-delBtn = document.getElementsByClassName("deleteButton");
-delTask = document.getElementsByClassName("myUl");
+// let delBtn, delTask;
+// delBtn = document.getElementsByClassName("deleteButton");
+// delTask = document.getElementsByClassName("myUl");
+//
+// for (let i = 0; i < delBtn.length; i++) {
+//     delBtn[i].onclick = function () {
+//         let div = this.delTask;
+//         div.style.display = "none";
+//     }
+// }
 
-for (let i = 0; i < delBtn.length; i++) {
-    delBtn[i].onclick = function () {
-        let div = this.delTask;
-        div.style.display = "none";
+let close = document.getElementsByClassName("deleteButton");
+let i;
+let result;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        if (confirm('Delete task?')) {
+           var div = this.parentElement;
+            div.style.display = "none";
+        }else (alert("task has not been deleted"));
+
     }
 }
